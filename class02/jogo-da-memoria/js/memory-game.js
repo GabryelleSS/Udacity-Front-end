e57538706b41057d04312f4e82eed68b1de11cd7
Gabryelle Silva Santos <gabryelle.santos@vianuvem.com.br>
2019-05-28 15:18:52 -0300

    Arrumando funcao de reinicar jogo

--- class02/jogo-da-memoria/js/memory-game.js
+++ class02/jogo-da-memoria/js/memory-game.js
@@ -1,35 +1,52 @@
 const memoryGame = (function() {
 
     let letters = document.querySelectorAll('li');
-    let numberPlays = 0;
+    let  numberCardsPlays = document.querySelector('span')
+    // let numberPlays = 0;
 
     function _listLetters() {
         letters.forEach(function(letter) {
             letter.addEventListener('click', function() {
                 letter.classList.toggle('--is-visible');
             })
+            _quantityPlays(0);
         })
-        _quantityPlays();
     }
 
-    function _quantityPlays() {
-        let numberCardsPlays = document.querySelector('span')
+    function _quantityPlays(numberPlays) {
         letters.forEach(function(letter) {
             letter.addEventListener('click', function() {
                 numberPlays++;
 
-                if(numberPlays > 1) {
-                    numberCardsPlays.textContent = `${numberPlays} Moves`
+                if(numberPlays === 0) {
+                    numberCardsPlays.textContent = " "
+                    console.log(numberPlays)
+                }
+                if(numberPlays === 1) {
+                    numberCardsPlays.textContent = `${numberPlays} Move`
                 }
                 else {
-                    numberCardsPlays.textContent = `${numberPlays} Move`
+                    numberCardsPlays.textContent = `${numberPlays} Moves`
                 }
             })
         })
     }
 
+    function _refresh() {
+        let arrowRefresh = document.querySelector('.arrow-refresh');
+
+        arrowRefresh.addEventListener('click', function() {
+            letters.forEach(function(letter) {
+                letter.classList.remove('--is-visible');
+            })
+            _quantityPlays(0);
+            numberCardsPlays.textContent = " ";
+        })
+    }
+
     function init() {
         _listLetters();
+        _refresh();
     }
 
     return {
