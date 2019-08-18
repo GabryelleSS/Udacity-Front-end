4dd10f44705666989cf810e3137c5a1ad1341367
Gabryelle SS <gabryelle-gaby@hotmail.com>
2019-08-18 15:44:39 -0300

    Adding description

--- class05/Restaurant/js/serveWorker.js
+++ class05/Restaurant/js/serveWorker.js
@@ -25,7 +25,7 @@ const cacheFiles = [
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open('v1').then(function (cache) {
-            return cache.addAll(cacheFiles)
+            return cache.addAll(cacheFiles);
         })
     );
 });
